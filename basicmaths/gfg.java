4783696054955c62165c8824f034810966a1d806
KARAN <110051623+Karanloganathan@users.noreply.github.com>
2023-03-04 08:56:21 +0530

    Add files via upload

---
+++ basicmaths/gfg.java
@@ -0,0 +1,12 @@
+class gfg{
+    static String compareNM(int n,int m){
+        // code here
+        if(n<m){
+            return "lesser";
+        }else if(n==m){
+            return "equal";
+            
+        }else{
+            return "greater";
+        }
+    }
